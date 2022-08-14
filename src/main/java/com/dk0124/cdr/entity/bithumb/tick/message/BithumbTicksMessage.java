c92d2e83d65aa5f7db6ceb2a637dc4de946000b9
dkGithup2022@gmail.com <dkGithup2022@gmail.com>
2022-08-14 23:53:12 +0900

    tick test 깨지던 것 수정 : id 를 upbit 에서 공급 받는 경우

--- src/main/java/com/dk0124/cdr/entity/bithumb/tick/message/BithumbTicksMessage.java
+++ src/main/java/com/dk0124/cdr/entity/bithumb/tick/message/BithumbTicksMessage.java
@@ -1,6 +1,6 @@
 package com.dk0124.cdr.entity.bithumb.tick.message;
 
-import com.dk0124.cdr.entity.bithumb.tick.BithumbTick;
+import com.dk0124.cdr.dto.bithumb.tick.BithumbTickDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -9,5 +9,5 @@ import java.util.List;
 @Data
 public class BithumbTicksMessage {
     @JsonProperty("list")
-    private List<BithumbTick> list;
+    private List<BithumbTickDto> list;
 }
